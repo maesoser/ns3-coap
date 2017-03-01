92fda3ce6da6a8af77d692866c25f490d41cbc26
maesoser <massesos@gmail.com>
2017-03-01 19:25:57 +0100

    Added scripts for the new simulations

--- alltest.sh
+++ alltest.sh
@@ -1,47 +1,6 @@
-./run --routing=0 --cache=1 --verbose=1 --mdns=1 --ping=1 --speed=0 --interval=15
-./run --routing=0 --cache=1 --verbose=1 --mdns=1 --ping=1 --speed=0 --interval=30
-./run --routing=0 --cache=1 --verbose=1 --mdns=1 --ping=1 --speed=0 --interval=60
-./run --routing=0 --cache=1 --verbose=1 --mdns=1 --ping=1 --speed=0 --interval=90
-./run --routing=0 --cache=1 --verbose=1 --mdns=1 --ping=1 --speed=0 --interval=120
-
-./run --routing=0 --cache=1 --verbose=1 --mdns=1 --ping=1 --speed=2 --interval=30
-./run --routing=0 --cache=1 --verbose=1 --mdns=1 --ping=1 --speed=5 --interval=30
-./run --routing=0 --cache=1 --verbose=1 --mdns=1 --ping=1 --speed=10 --interval=30
-./run --routing=0 --cache=1 --verbose=1 --mdns=1 --ping=1 --speed=20 --interval=30
-
-./run --routing=1 --cache=1 --verbose=1 --mdns=1 --ping=1 --speed=0 --interval=15
-./run --routing=1 --cache=1 --verbose=1 --mdns=1 --ping=1 --speed=0 --interval=30
-./run --routing=1 --cache=1 --verbose=1 --mdns=1 --ping=1 --speed=0 --interval=60
-./run --routing=1 --cache=1 --verbose=1 --mdns=1 --ping=1 --speed=0 --interval=90
-./run --routing=1 --cache=1 --verbose=1 --mdns=1 --ping=1 --speed=0 --interval=120
-
-./run --routing=1 --cache=1 --verbose=1 --mdns=1 --ping=1 --speed=2 --interval=30
-./run --routing=1 --cache=1 --verbose=1 --mdns=1 --ping=1 --speed=5 --interval=30
-./run --routing=1 --cache=1 --verbose=1 --mdns=1 --ping=1 --speed=10 --interval=30
-./run --routing=1 --cache=1 --verbose=1 --mdns=1 --ping=1 --speed=20 --interval=30
-
-./run --routing=0 --cache=1 --verbose=1 --ping=1 --speed=0 --interval=15
-./run --routing=0 --cache=1 --verbose=1 --ping=1 --speed=0 --interval=30
-./run --routing=0 --cache=1 --verbose=1 --ping=1 --speed=0 --interval=60
-./run --routing=0 --cache=1 --verbose=1 --ping=1 --speed=0 --interval=90
-./run --routing=0 --cache=1 --verbose=1 --ping=1 --speed=0 --interval=120
-
-./run --routing=0 --cache=1 --verbose=1 --ping=1 --speed=2 --interval=30
-./run --routing=0 --cache=1 --verbose=1 --ping=1 --speed=5 --interval=30
-./run --routing=0 --cache=1 --verbose=1 --ping=1 --speed=10 --interval=30
-./run --routing=0 --cache=1 --verbose=1 --ping=1 --speed=20 --interval=30
-
-./run --routing=1 --cache=1 --verbose=1 --ping=1 --speed=0 --interval=15
-./run --routing=1 --cache=1 --verbose=1 --ping=1 --speed=0 --interval=30
-./run --routing=1 --cache=1 --verbose=1 --ping=1 --speed=0 --interval=60
-./run --routing=1 --cache=1 --verbose=1 --ping=1 --speed=0 --interval=90
-./run --routing=1 --cache=1 --verbose=1 --ping=1 --speed=0 --interval=120
-
-./run --routing=1 --cache=1 --verbose=1 --ping=1 --speed=2 --interval=30
-./run --routing=1 --cache=1 --verbose=1 --ping=1 --speed=5 --interval=30
-./run --routing=1 --cache=1 --verbose=1 --ping=1 --speed=10 --interval=30
-./run --routing=1 --cache=1 --verbose=1 --ping=1 --speed=20 --interval=30
-
-rm experiments.zip
-zip experiments routing_*
-rm routing_*
+sh ucast_nocache.sh
+sh ucast_cache.sh
+sh mcast_nocache.sh
+sh mcast_cache.sh
+sh mcast_cache_stime.sh
+curl -X POST https://maker.ifttt.com/trigger/exp_finish/with/key/bGMCoV-2UgI-PDoi08UOXi
---
+++ mcast_cache.sh
@@ -0,0 +1,69 @@
+# NONE MCAST NOCACHE MDNS
+./run --routing=0 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=1 --speed=0 --interval=15
+./run --routing=0 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=1 --speed=0 --interval=30
+./run --routing=0 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=1 --speed=0 --interval=60
+./run --routing=0 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=1 --speed=0 --interval=90
+./run --routing=0 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=1 --speed=0 --interval=120
+./run --routing=0 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=1 --speed=2 --interval=30
+./run --routing=0 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=1 --speed=5 --interval=30
+./run --routing=0 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=1 --speed=10 --interval=30
+./run --routing=0 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=1 --speed=20 --interval=30
+
+# NONE MCAST NOCACHE COAP
+./run --routing=0 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=0 --speed=0 --interval=15
+./run --routing=0 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=0 --speed=0 --interval=30
+./run --routing=0 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=0 --speed=0 --interval=60
+./run --routing=0 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=0 --speed=0 --interval=90
+./run --routing=0 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=0 --speed=0 --interval=120
+./run --routing=0 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=0 --speed=2 --interval=30
+./run --routing=0 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=0 --speed=5 --interval=30
+./run --routing=0 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=0 --speed=10 --interval=30
+./run --routing=0 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=0 --speed=20 --interval=30
+
+# SMF MCAST NOCACHE MDNS
+./run --routing=1 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=1 --speed=0 --interval=15
+./run --routing=1 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=1 --speed=0 --interval=30
+./run --routing=1 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=1 --speed=0 --interval=60
+./run --routing=1 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=1 --speed=0 --interval=90
+./run --routing=1 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=1 --speed=0 --interval=120
+./run --routing=1 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=1 --speed=2 --interval=30
+./run --routing=1 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=1 --speed=5 --interval=30
+./run --routing=1 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=1 --speed=10 --interval=30
+./run --routing=1 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=1 --speed=20 --interval=30
+
+# SMF MCAST NOCA1HE COAP
+./run --routing=1 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=0 --speed=0 --interval=15
+./run --routing=1 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=0 --speed=0 --interval=30
+./run --routing=1 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=0 --speed=0 --interval=60
+./run --routing=1 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=0 --speed=0 --interval=90
+./run --routing=1 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=0 --speed=0 --interval=120
+./run --routing=1 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=0 --speed=2 --interval=30
+./run --routing=1 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=0 --speed=5 --interval=30
+./run --routing=1 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=0 --speed=10 --interval=30
+./run --routing=1 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=0 --speed=20 --interval=30
+
+# OLSR MCAST NOCACHE MDNS
+./run --routing=2 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=1 --speed=0 --interval=15
+./run --routing=2 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=1 --speed=0 --interval=30
+./run --routing=2 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=1 --speed=0 --interval=60
+./run --routing=2 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=1 --speed=0 --interval=90
+./run --routing=2 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=1 --speed=0 --interval=120
+./run --routing=2 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=1 --speed=2 --interval=30
+./run --routing=2 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=1 --speed=5 --interval=30
+./run --routing=2 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=1 --speed=10 --interval=30
+./run --routing=2 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=1 --speed=20 --interval=30
+
+# OLSR MCAST NOCAHE COAP
+./run --routing=2 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=0 --speed=0 --interval=15
+./run --routing=2 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=0 --speed=0 --interval=30
+./run --routing=2 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=0 --speed=0 --interval=60
+./run --routing=2 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=0 --speed=0 --interval=90
+./run --routing=2 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=0 --speed=0 --interval=120
+./run --routing=2 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=0 --speed=2 --interval=30
+./run --routing=2 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=0 --speed=5 --interval=30
+./run --routing=2 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=0 --speed=10 --interval=30
+./run --routing=2 --ping=1 --verbose=1 --cache=1 --mcast=1 --stime=0 --mdns=0 --speed=20 --interval=30
+
+rm experiments.zip
+zip mcast_cache routing_*
+rm routing_*
